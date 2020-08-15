a8c2965c2b30c57aa5abfe1b04b2811983b8f545
Rahul Dhanasiri <dhanasirirahul@hotmail.com>
2020-08-15 12:47:51 -0500

    Updates

---
+++ precache-manifest.be5f238dcf882f7cee169e95ba52715a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c16353c20c337c97b901351a6b9c1bd",
+    "url": "/React-youtube-Api/index.html"
+  },
+  {
+    "revision": "0d4c1c802bef4723dd00",
+    "url": "/React-youtube-Api/static/css/main.e68f5f3a.chunk.css"
+  },
+  {
+    "revision": "35e5d30d56b02a9f4bad",
+    "url": "/React-youtube-Api/static/js/2.2605bfb9.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/React-youtube-Api/static/js/2.2605bfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4c1c802bef4723dd00",
+    "url": "/React-youtube-Api/static/js/main.78e70a8e.chunk.js"
+  },
+  {
+    "revision": "8876c6e10d3c6ce2dfe6",
+    "url": "/React-youtube-Api/static/js/runtime-main.ed7df3c7.js"
+  }
+]);
