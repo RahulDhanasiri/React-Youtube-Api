62bc3017b3f761860f2445ef255ba722936154d6
Rahul Dhanasiri <dhanasirirahul@hotmail.com>
2020-08-15 13:52:50 -0500

    Updates

---
+++ precache-manifest.966554c77dfff1ff161f8041ca9e00a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e01599a54aabb0560d4351f2076e7d8",
+    "url": "/React-Youtube-Api/index.html"
+  },
+  {
+    "revision": "0d4c1c802bef4723dd00",
+    "url": "/React-Youtube-Api/static/css/main.e68f5f3a.chunk.css"
+  },
+  {
+    "revision": "35e5d30d56b02a9f4bad",
+    "url": "/React-Youtube-Api/static/js/2.2605bfb9.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/React-Youtube-Api/static/js/2.2605bfb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d4c1c802bef4723dd00",
+    "url": "/React-Youtube-Api/static/js/main.78e70a8e.chunk.js"
+  },
+  {
+    "revision": "2c8109cb0e8a57ce8710",
+    "url": "/React-Youtube-Api/static/js/runtime-main.0978104c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-youtube-Api/precache-manifest.be5f238dcf882f7cee169e95ba52715a.js"
+  "/React-Youtube-Api/precache-manifest.966554c77dfff1ff161f8041ca9e00a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-youtube-Api/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Youtube-Api/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
